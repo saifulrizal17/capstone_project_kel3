2e4f129530d162349693b81c8d88d06cec8c1d6d
Rajendra-Anargya <rajendra.anargya@gmail.com>
2023-12-13 11:35:40 +0700

    menghapus yang tidak pasti

--- resources/views/frondend/index.blade.php
+++ resources/views/frondend/index.blade.php
@@ -29,7 +29,6 @@
                         Selamat datang di Sejahtera.id, kawan setia dalam perjalanan Anda menuju kebebasan
                         finansial. Kami adalah pelopor solusi keuangan yang membawa perubahan positif dalam cara
                         Anda mengelola dan merencanakan keuangan pribadi.
-                        -Nyoba
                     </p>
                     <a href="#" class="btn-learn-more">Learn More</a>
                 </div>
@@ -126,75 +125,6 @@
 
         </div>
     </section>
-
-    {{-- <section id="pricing" class="pricing">
-        <div class="container">
-
-            <div class="section-title">
-                <h2>Pricing</h2>
-                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
-                    consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat
-                    sit in iste officiis commodi quidem hic quas.</p>
-            </div>
-
-            <div class="row">
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="box recommended">
-                        <h3>Free</h3>
-                        <span class="recommended-badge">Recommended</span>
-                        <h4><sup>$</sup>0<span> / month</span></h4>
-                        <ul>
-                            <li>Aida dere</li>
-                            <li>Nec feugiat nisl</li>
-                            <li>Nulla at volutpat dola</li>
-                            <li class="na">Pharetra massa</li>
-                            <li class="na">Massa ultricies mi</li>
-                        </ul>
-                        <div class="btn-wrap">
-                            <a href="#" class="btn-buy">Buy Now</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
-                    <div class="box">
-                        <h3>Business</h3>
-                        <h4><sup>$</sup>19<span> / month</span></h4>
-                        <ul>
-                            <li>Aida dere</li>
-                            <li>Nec feugiat nisl</li>
-                            <li>Nulla at volutpat dola</li>
-                            <li>Pharetra massa</li>
-                            <li class="na">Massa ultricies mi</li>
-                        </ul>
-                        <div class="btn-wrap">
-                            <a href="#" class="btn-buy">Buy Now</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
-                    <div class="box">
-                        <h3>Developer</h3>
-                        <h4><sup>$</sup>29<span> / month</span></h4>
-                        <ul>
-                            <li>Aida dere</li>
-                            <li>Nec feugiat nisl</li>
-                            <li>Nulla at volutpat dola</li>
-                            <li>Pharetra massa</li>
-                            <li>Massa ultricies mi</li>
-                        </ul>
-                        <div class="btn-wrap">
-                            <a href="#" class="btn-buy">Buy Now</a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-    </section> --}}
 
     <section id="team" class="team">
         <div class="container" data-aos="fade-up">
